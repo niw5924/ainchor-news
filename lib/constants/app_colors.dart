6048b62b7a07047c0892be3ee6a01bbc69f2c371
niw5924 <niw5924@naver.com>
2025-09-30 22:45:11 +0900

    chore: 카드 백그라운드 색상 변경

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -7,5 +7,5 @@ class AppColors {
   static const Color appBarBackground = Color(0xFFD6E3F5);
   static const Color scaffoldBackground = Color(0xFFEEF2F7);
   static const Color bottomNavBackground = Color(0xFFE6EDF7);
-  static const Color cardBackground = Color(0xFFFAFCFF);
+  static const Color cardBackground = Color(0xFFFEFEFF);
 }
