eadbcab33a15219a6ca93783c46f6941941bf421
niw5924 <niw5924@naver.com>
2025-10-01 23:22:20 +0900

    feat: Rive 애니메이션 배치

--- lib/anchor/anchor_screen.dart
+++ lib/anchor/anchor_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class AnchorScreen extends StatelessWidget {
   const AnchorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('앵커 탭'));
+    return const Center(
+      child: RiveAnimation.asset(
+        'assets/rives/jihye_anchor.riv',
+        fit: BoxFit.contain,
+      ),
+    );
   }
 }
