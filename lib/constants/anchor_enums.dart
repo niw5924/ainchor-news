d651a034e2820883be1ed43d2c341e014b97991b
niw5924 <niw5924@naver.com>
2025-10-13 23:39:46 +0900

    feat: anchor_enums required로 변경

--- lib/constants/anchor_enums.dart
+++ lib/constants/anchor_enums.dart
@@ -1,11 +1,31 @@
 /// 앵커 프로필
 enum Anchor {
-  jihye('지혜', 'Jihye', '밝고 명료한 톤'),
-  seoyeon('서연', 'Seoyeon', '차분하고 따뜻한 톤');
+  jihye(
+    nameKo: '지혜',
+    nameEn: 'Jihye',
+    voiceStyle: '밝고 명료한 톤',
+    rivePath: 'assets/rives/jihye_anchor.riv',
+    audioPath: 'assets/audios/jihye_sample.mp3',
+  ),
+  seoyeon(
+    nameKo: '서연',
+    nameEn: 'Seoyeon',
+    voiceStyle: '차분하고 따뜻한 톤',
+    rivePath: 'assets/rives/seoyeon_anchor.riv',
+    audioPath: 'assets/audios/seoyeon_sample.mp3',
+  );
 
-  const Anchor(this.nameKo, this.nameEn, this.voiceStyle);
+  const Anchor({
+    required this.nameKo,
+    required this.nameEn,
+    required this.voiceStyle,
+    required this.rivePath,
+    required this.audioPath,
+  });
 
   final String nameKo;
   final String nameEn;
   final String voiceStyle;
+  final String rivePath;
+  final String audioPath;
 }
