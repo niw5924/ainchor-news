7050d4703ffd7fbe2cd09a0e8efbef51ff2d5243
niw5924 <niw5924@naver.com>
2025-11-01 13:02:38 +0900

    model: 네이버 뉴스 모델 파싱

--- lib/models/naver_news_model.dart
+++ lib/models/naver_news_model.dart
@@ -1,25 +1,26 @@
+import '../utils/date_time_utils.dart';
+import '../utils/html_utils.dart';
+
 class NaverNewsModel {
   final String title;
   final String description;
   final String pubDate;
-  final String originallink;
   final String link;
 
   const NaverNewsModel({
     required this.title,
     required this.description,
     required this.pubDate,
-    required this.originallink,
     required this.link,
   });
 
   factory NaverNewsModel.fromJson(Map<String, dynamic> json) {
     return NaverNewsModel(
-      title: json['title'],
-      description: json['description'],
-      pubDate: json['pubDate'],
-      originallink: json['originallink'],
-      link: json['link'],
+      title: HtmlUtils.parseHtmlString(json['title']),
+      description: HtmlUtils.parseHtmlString(json['description']),
+      pubDate: DateTimeUtils.parsePubDateString(json['pubDate']),
+      link:
+          json['originallink'].isNotEmpty ? json['originallink'] : json['link'],
     );
   }
 }
