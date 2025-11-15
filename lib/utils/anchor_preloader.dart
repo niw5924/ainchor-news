cf45134236f6e43d7b01c595f00c78d4f16aa387
niw5924 <niw5924@naver.com>
2025-11-15 22:26:12 +0900

    refactor: 앵커 Rive 파일 static 프리로더로 변경

--- lib/utils/anchor_preloader.dart
+++ lib/utils/anchor_preloader.dart
@@ -1,41 +1,19 @@
 import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
+
 import '../constants/anchor_enums.dart';
 
-/// 런타임 캐시 단일화·전역 오염 방지용 싱글턴(외부 생성 금지)
 class AnchorPreloader {
-  /// 외부 생성 금지
-  AnchorPreloader._();
+  static final Map<int, RiveFile> riveFiles = {};
 
-  /// 싱글턴 인스턴스
-  static final AnchorPreloader instance = AnchorPreloader._();
-
-  final Map<int, Artboard> artboards = {};
-  final Map<int, SMIInput<bool>> talkingInputs = {};
-
-  Future<void> preloadAllAnchors() async {
+  static Future<void> preloadAllAnchors() async {
     await RiveFile.initialize();
+
     for (int index = 0; index < Anchor.values.length; index++) {
       final anchor = Anchor.values[index];
       final riveBytes = await rootBundle.load(anchor.rivePath);
       final riveFile = RiveFile.import(riveBytes);
-
-      final artboard = riveFile.mainArtboard;
-      final controller = StateMachineController.fromArtboard(
-        artboard,
-        'State Machine',
-      );
-
-      if (controller != null) {
-        artboard.addController(controller);
-        final talkingInput = controller.findInput<bool>('isTalking');
-        if (talkingInput != null) {
-          talkingInput.value = false;
-          talkingInputs[index] = talkingInput;
-        }
-      }
-
-      artboards[index] = artboard;
+      riveFiles[index] = riveFile;
     }
   }
 }
