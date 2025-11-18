36cc984a6865d305a86729b236b9238572b7dc82
niw5924 <niw5924@naver.com>
2025-11-18 23:40:19 +0900

    fix: 설정 화면 SingleChildScrollView

--- lib/settings/settings_screen.dart
+++ lib/settings/settings_screen.dart
@@ -8,29 +8,39 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("1줄")),
-        const Divider(color: AppColors.divider),
-        const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("2줄")),
-        const Divider(color: AppColors.divider),
-        const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("3줄")),
-        const Divider(color: AppColors.divider),
-        const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("4줄")),
-        const Divider(color: AppColors.divider),
-        const AinchorListTile(
-          title: Text("뉴스 길이"),
-          subtitle: Text("5줄"),
-          showTrailing: false,
-        ),
-        const Divider(color: AppColors.divider),
-        AinchorListTile(
-          title: const Text("오픈소스 라이선스"),
-          onTap: () {
-            showLicensePage(context: context, useRootNavigator: true);
-          },
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("1줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("2줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("3줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("4줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(
+            title: Text("뉴스 길이"),
+            subtitle: Text("5줄"),
+            showTrailing: false,
+          ),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("6줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("7줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("8줄")),
+          const Divider(color: AppColors.divider),
+          const AinchorListTile(title: Text("뉴스 길이"), subtitle: Text("9줄")),
+          const Divider(color: AppColors.divider),
+          AinchorListTile(
+            title: const Text("오픈소스 라이선스"),
+            onTap: () {
+              showLicensePage(context: context, useRootNavigator: true);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
