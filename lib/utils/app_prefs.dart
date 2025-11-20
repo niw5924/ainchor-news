5b471229491bd4854ef1bbb43070c96e4defdcf4
niw5924 <niw5924@naver.com>
2025-11-20 22:40:37 +0900

    refactor: AppPrefs를 ValueNotifier 기반 리액티브 구조로 변경

--- lib/utils/app_prefs.dart
+++ lib/utils/app_prefs.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppPrefsKeys {
@@ -10,7 +11,13 @@ class AppPrefsDefaults {
   static const summaryCount = 5;
 }
 
-/// 정적 prefs 유틸: 인스턴스 생성 금지(오용 방지)
+/// 앱 prefs ValueNotifier 상태 (UI 연동용)
+class AppPrefsState {
+  static late final ValueNotifier<String> anchor;
+  static late final ValueNotifier<int> summaryCount;
+}
+
+/// 정적 prefs 유틸: 인스턴스 생성 금지 (오용 방지)
 class AppPrefs {
   AppPrefs._();
 
@@ -19,6 +26,16 @@ class AppPrefs {
   /// 앱 시작 시 1회 초기화
   static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
+
+    final savedAnchor = _prefs!.getString(AppPrefsKeys.anchor);
+    final savedSummaryCount = _prefs!.getInt(AppPrefsKeys.summaryCount);
+
+    AppPrefsState.anchor = ValueNotifier<String>(
+      savedAnchor ?? AppPrefsDefaults.anchor,
+    );
+    AppPrefsState.summaryCount = ValueNotifier<int>(
+      savedSummaryCount ?? AppPrefsDefaults.summaryCount,
+    );
   }
 
   /// 값 저장
@@ -43,6 +60,16 @@ class AppPrefs {
       default:
         break;
     }
+
+    /// ValueNotifier 동기화
+    switch (key) {
+      case AppPrefsKeys.anchor:
+        AppPrefsState.anchor.value = value as String;
+        break;
+      case AppPrefsKeys.summaryCount:
+        AppPrefsState.summaryCount.value = value as int;
+        break;
+    }
   }
 
   /// 값 조회 (없으면 null)
@@ -53,5 +80,15 @@ class AppPrefs {
   /// 키 삭제
   static Future<void> remove(String key) async {
     await _prefs!.remove(key);
+
+    /// 삭제 시 기본값으로 되돌리기
+    switch (key) {
+      case AppPrefsKeys.anchor:
+        AppPrefsState.anchor.value = AppPrefsDefaults.anchor;
+        break;
+      case AppPrefsKeys.summaryCount:
+        AppPrefsState.summaryCount.value = AppPrefsDefaults.summaryCount;
+        break;
+    }
   }
 }
